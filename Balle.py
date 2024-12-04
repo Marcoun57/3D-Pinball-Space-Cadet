cb3aa67fc29a1b5bd3e4afd49610dd7a29848c27
Marc <marc.bastian@protonmail.com>
2024-12-04 11:05:29 +0100

    Update Balle.py

--- Balle.py
+++ Balle.py
@@ -6,6 +6,11 @@ pygame.init()
 # Définir les dimensions de l'écran
 WIDTH = 800
 HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Couleurs
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 # Paramètres de la balle
 ball_radius = 15
